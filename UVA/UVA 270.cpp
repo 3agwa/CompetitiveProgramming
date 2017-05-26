07b479dfc320c9d3d2145f56bac6d47e2ccc7943
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-05-26 16:57:29 +0200

    Add files via upload

---
+++ UVA/UVA 270.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+
+/*
+    counting the slope of point Xi with points Xj where j > i
+    using maps to count the given slopes and maximize over the greatest count
+    N^2 approach excluding test case loop
+
+    got TLE then WA due to taking input incorrectly
+*/
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-8)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+double INF = 1e100;
+double EPS = 1e-7;
+
+struct PT
+{
+    double x, y;
+    PT() {}
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &p)  const
+    {
+        return PT(x+p.x, y+p.y);
+    }
+    PT operator - (const PT &p)  const
+    {
+        return PT(x-p.x, y-p.y);
+    }
+    PT operator * (double c)     const
+    {
+        return PT(x*c,   y*c  );
+    }
+    PT operator / (double c)     const
+    {
+        return PT(x/c,   y/c  );
+    }
+    bool operator<(const PT &p)  const
+    {
+        return make_pair(x,y)<make_pair(p.x,p.y);
+    }
+    bool operator==(const PT &p)  const
+    {
+        return !(*this < p) && !(p < *this);
+    }
+};
+double dot(PT p, PT q)
+{
+    return p.x*q.x+p.y*q.y;
+}
+double dist2(PT p, PT q)
+{
+    return dot(p-q,p-q);
+}
+double cross(PT p, PT q)
+{
+    return p.x*q.y-p.y*q.x;
+}
+PT norm(PT x, double l)
+{
+    return x * sqrt(l*l / dot(x,x));
+}
+istream &operator>>(istream &is, PT &p)
+{
+    return is >> p.x >> p.y;
+}
+ostream &operator<<(ostream &os, const PT &p)
+{
+    return os << "(" << p.x << "," << p.y << ")";
+}
+/*around the origin*/
+PT RotateCCW90(PT p)
+{
+    return PT(-p.y,p.x);
+}
+PT RotateCW90(PT p)
+{
+    return PT(p.y,-p.x);
+}
+PT RotateCCW(PT p, double t)
+{
+    return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));
+}
+// project point c onto line through a and b (assuming a != b)
+PT ProjectPointLine(PT a, PT b, PT c)
+{
+    return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
+}
+// project point c onto line segment through a and b (assuming a != b)
+PT ProjectPointSegment(PT a, PT b, PT c)
+{
+    double r = dot(c-a, b-a)/dot(b-a,b-a);
+    if (r < 0) return a;
+    if (r > 1) return b;
+    return a + (b-a)*r;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+double slope(PT a, PT b)
+{
+    if (a.x - b.x == 0) return INT_MAX;
+    return (a.y - b.y) / (a.x - b.x);
+}
+
+vector<PT> vec;
+
+int main()
+{
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    int tc;
+    scanf("%d\n",&tc);
+    while(tc--)
+    {
+        vec.clear();
+        string s;
+        while(getline(cin,s))
+        {
+            if(s=="")
+                break;
+            stringstream ss;
+            ss<<s;
+            int x,y;
+            ss>>x>>y;
+            vec.push_back(PT(x,y));
+        }
+        int mx = 0;
+        rep(i, 0, sz(vec))
+        {
+            map< ld, int > mp;
+            rep(j, i+1, sz(vec))
+            {
+                ld hoba = slope(vec[i], vec[j]);
+                mp[hoba]++;
+                mx = max(mp[hoba]+1, mx);
+            }
+        }
+        printf("%d\n", mx);
+        if (tc) printf("\n");
+    }
+
+    return 0;
+}
