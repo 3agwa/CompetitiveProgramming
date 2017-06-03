fb9e765726845baef992bc0b2653ff0f5cc338e5
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-06-03 14:28:57 +0200

    Add files via upload

---
+++ Hackerrank/HACKR baby-step-giant-step.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-8)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+
+double INF = 1e100;
+double EPS = 1e-7;
+
+struct PT
+{
+    double x, y;
+    PT() {}
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &p)  const
+    {
+        return PT(x+p.x, y+p.y);
+    }
+    PT operator - (const PT &p)  const
+    {
+        return PT(x-p.x, y-p.y);
+    }
+    PT operator * (double c)     const
+    {
+        return PT(x*c,   y*c  );
+    }
+    PT operator / (double c)     const
+    {
+        return PT(x/c,   y/c  );
+    }
+    bool operator<(const PT &p)  const
+    {
+        return make_pair(x,y)<make_pair(p.x,p.y);
+    }
+    bool operator==(const PT &p)  const
+    {
+        return !(*this < p) && !(p < *this);
+    }
+};
+double dot(PT p, PT q)
+{
+    return sqrt(p.x*q.x+p.y*q.y);
+}
+double dist2(PT p, PT q)
+{
+    return dot(p-q,p-q);
+}
+double cross(PT p, PT q)
+{
+    return p.x*q.y-p.y*q.x;
+}
+PT norm(PT x, double l)
+{
+    return x * sqrt(l*l / dot(x,x));
+}
+istream &operator>>(istream &is, PT &p)
+{
+    return is >> p.x >> p.y;
+}
+ostream &operator<<(ostream &os, const PT &p)
+{
+    return os << "(" << p.x << "," << p.y << ")";
+}
+/*around the origin*/
+PT RotateCCW90(PT p)
+{
+    return PT(-p.y,p.x);
+}
+PT RotateCW90(PT p)
+{
+    return PT(p.y,-p.x);
+}
+PT RotateCCW(PT p, double t)
+{
+    return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));
+}
+// project point c onto line through a and b (assuming a != b)
+PT ProjectPointLine(PT a, PT b, PT c)
+{
+    return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
+}
+// project point c onto line segment through a and b (assuming a != b)
+PT ProjectPointSegment(PT a, PT b, PT c)
+{
+    double r = dot(c-a, b-a)/dot(b-a,b-a);
+    if (r < 0) return a;
+    if (r > 1) return b;
+    return a + (b-a)*r;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        double a, b, d, cnt = 0;
+        int x = 0;
+        cin >> a >> b >> d;
+        if (d >= b)
+        {
+            cout << ceil(d/b) << endl;
+            continue;
+        }
+        else if (!d)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        else if (d == a)
+        {
+            cout << 1 << endl;
+            continue;
+        }
+        cout << 2 << endl;
+
+    }
+    return 0;
+}
