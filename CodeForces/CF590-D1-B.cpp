795fee2a69226e994e6f06b5cb57e1f89bf217d0
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-06-18 23:09:19 +0200

    Add files via upload

---
+++ CodeForces/CF590-D1-B.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-8)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+
+double INF = 1e100;
+double EPS = 1e-7;
+
+struct PT
+{
+    double x, y;
+    PT() {}
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &p)  const
+    {
+        return PT(x+p.x, y+p.y);
+    }
+    PT operator - (const PT &p)  const
+    {
+        return PT(x-p.x, y-p.y);
+    }
+    PT operator * (double c)     const
+    {
+        return PT(x*c,   y*c  );
+    }
+    PT operator / (double c)     const
+    {
+        return PT(x/c,   y/c  );
+    }
+    bool operator<(const PT &p)  const
+    {
+        return make_pair(x,y)<make_pair(p.x,p.y);
+    }
+    bool operator==(const PT &p)  const
+    {
+        return !(*this < p) && !(p < *this);
+    }
+};
+double dot(PT p, PT q)
+{
+    return p.x*q.x+p.y*q.y;
+}
+double dist2(PT p, PT q)
+{
+    return dot(p-q,p-q);
+}
+double cross(PT p, PT q)
+{
+    return p.x*q.y-p.y*q.x;
+}
+PT norm(PT x, double l)
+{
+    return x * sqrt(l*l / dot(x,x));
+}
+istream &operator>>(istream &is, PT &p)
+{
+    return is >> p.x >> p.y;
+}
+ostream &operator<<(ostream &os, const PT &p)
+{
+    return os << "(" << p.x << "," << p.y << ")";
+}
+/*around the origin*/
+PT RotateCCW90(PT p)
+{
+    return PT(-p.y,p.x);
+}
+PT RotateCW90(PT p)
+{
+    return PT(p.y,-p.x);
+}
+PT RotateCCW(PT p, double t)
+{
+    return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));
+}
+// project point c onto line through a and b (assuming a != b)
+PT ProjectPointLine(PT a, PT b, PT c)
+{
+    return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
+}
+// project point c onto line segment through a and b (assuming a != b)
+PT ProjectPointSegment(PT a, PT b, PT c)
+{
+    double r = dot(c-a, b-a)/dot(b-a,b-a);
+    if (r < 0) return a;
+    if (r > 1) return b;
+    return a + (b-a)*r;
+}
+
+PT s, e, v, w;
+double n, t;
+double total;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> s >> e;
+    cin >> n >> t;
+    cin >> v >> w;
+    total = dist2(s, e);
+
+    double st = 0.0, en = 1e10, mid, cnt = 0;
+    while(cnt++ < 200)
+    {
+        mid = (st+en)/2.0;
+
+        double x = s.x + min(mid, t)*v.x + max(0.0, mid-t)*w.x;
+        double y = s.y + min(mid, t)*v.y + max(0.0, mid-t)*w.y;
+        double dist = sqrt(dist2(PT(x, y), e));
+        cout << PT(x, y) << " " << e << " " << sqrt(dist) << " " << en << endl;
+        if (dist < n*mid) en = mid;
+        else st = mid;
+    }
+    cout << fixed << setprecision(9) << en;
+    return 0;
+}
