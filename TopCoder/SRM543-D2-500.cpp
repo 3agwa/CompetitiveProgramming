1ba286a461bf76deee4b4c088afffa5c27bb399c
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-06-21 14:25:34 +0200

    Add files via upload

---
+++ TopCoder/SRM543-D2-500.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-6)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+
+class EllysXors {
+public:
+    ll f(long long a)
+    {
+        long long res[] = {a,1,a+1,0};
+        return res[a%4];
+    }
+    ll getXor(long long L, long long R)
+    {
+		return f(R)^f(L-1);
+	}
+};
