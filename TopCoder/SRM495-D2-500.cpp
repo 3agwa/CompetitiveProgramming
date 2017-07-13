4c81c1dc663ceba279f55e866cc7a1995de20aea
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-07-13 15:50:29 +0200

    Update SRM495-D2-500.cpp

--- TopCoder/SRM495-D2-500.cpp
+++ TopCoder/SRM495-D2-500.cpp
@@ -1,3 +1,38 @@
+/*
+    check if there are more that 1 occurance of the sequence, if so check which elements don't fit
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-6)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -96,7 +131,7 @@ public:
         ret.clear();
         rep(i, 0, sz(colors)) ret.push_back(0);
 
-        erep(i, 1, N) solve(1, i);
+        solve(1, N);
 
         return ret;
     }
