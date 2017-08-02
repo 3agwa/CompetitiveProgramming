760a0f75ff6af08d95340f4b09d5d1777b8754d0
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-02 13:20:05 +0200

    Add files via upload

---
+++ UVA/UVA 563.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+//#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-8)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+const int INF = 2000000000;
+
+struct Edge
+{
+    int from, to, cap, flow, index;
+    Edge(int from, int to, int cap, int flow, int index) :
+        from(from), to(to), cap(cap), flow(flow), index(index) { }
+};
+
+struct Dinic
+{
+    int N;
+    vector<vector<Edge> > G;
+    vector<Edge *> dad;
+    vector<int> Q;
+
+    Dinic(int N) : N(N), G(N), dad(N), Q(N) { }
+
+    void AddEdge(int from, int to, int cap)
+    {
+        G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+        if(from == to) G[from].back().index++;
+        G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
+    }
+
+    long long BlockingFlow(int s, int t)
+    {
+        fill(dad.begin(), dad.end(), (Edge *)NULL);
+        dad[s] = &G[0][0] - 1;
+
+        int head = 0, tail = 0;
+        Q[tail++] = s;
+        while(head < tail)
+        {
+            int x = Q[head++];
+            for(int i = 0; i < G[x].size(); i++)
+            {
+                Edge &e = G[x][i];
+                if(!dad[e.to] && e.cap - e.flow > 0)
+                {
+                    dad[e.to] = &G[x][i];
+                    Q[tail++] = e.to;
+                }
+            }
+        }
+        if(!dad[t]) return 0;
+
+        long long totflow = 0;
+        for(int i = 0; i < G[t].size(); i++)
+        {
+            Edge *start = &G[G[t][i].to][G[t][i].index];
+            int amt = INF;
+            for(Edge *e = start; amt && e != dad[s]; e = dad[e->from])
+            {
+                if(!e)
+                {
+                    amt = 0;
+                    break;
+                }
+                amt = min(amt, e->cap - e->flow);
+            }
+            if(amt == 0) continue;
+            for(Edge *e = start; amt && e != dad[s]; e = dad[e->from])
+            {
+                e->flow += amt;
+                G[e->to][e->index].flow -= amt;
+            }
+            totflow += amt;
+        }
+        return totflow;
+    }
+
+    long long GetMaxFlow(int s, int t)
+    {
+        long long totflow = 0;
+        while(long long flow = BlockingFlow(s, t))
+            totflow += flow;
+        return totflow;
+    }
+};
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int mp[501][501];
+        reset(mp, 0);
+        int m, n, k;
+        cin >> m >> n >> k;
+        rep(i, 0, k)
+        {
+            int x, y;
+            cin >> x >> y;
+            mp[x-1][y-1]++;
+        }
+
+        int sink = 1 + (2*m*n), src = 0;
+        Dinic flow(32000);
+        rep(i, 0, m)
+        {
+            rep(j, 0, n)
+            {
+                int u = i*n + j;
+                int vertex1 = 1 + 2*u, vertex2 = vertex1 + 1;
+
+                flow.AddEdge(vertex1, vertex2, 1); // vertex splitting
+
+                if (!i || !j || i == m-1 || j == n-1) flow.AddEdge(vertex2, sink, 1);
+
+                if (mp[i][j]) flow.AddEdge(src, vertex1, mp[i][j]);
+
+                rep(k, 0, 4)
+                {
+                    int x = i + dx[k], y = j + dy[k];
+
+                    if (x >= 0 && x < m && y >= 0 && y < n)
+                    {
+                        int v = x * n + y;
+
+                        flow.AddEdge(vertex2, 1 + 2*v, 1); // my sink to node's source
+                    }
+                }
+            }
+        }
+
+        if (flow.GetMaxFlow(src, sink) == k) cout << "possible\n";
+        else cout << "not possible\n";
+    }
+
+    return 0;
+}
