90bb2a5b7cb1a25e7a8b7e0611c5613a66b1a54f
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-10 12:31:06 +0200

    Add files via upload

---
+++ TopCoder/SRM174-D1-250.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+class BirthdayOdds
+{
+public:
+    int minPeople(int minOdds, int daysInYear)
+    {
+        double p = 1, target = 1.0 - (double) minOdds / 100.0;
+        double cnt = 1;
+        while(p > target)
+        {
+            p = p * (1.0 - (double) cnt / daysInYear);
+            cnt++;
+        }
+        return cnt;
+    }
+};
+
