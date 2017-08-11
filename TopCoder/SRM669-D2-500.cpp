4b375036701d0c7a29c3ad9a54e5b3dd5c2de726
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-11 21:20:22 +0200

    Add files via upload

---
+++ TopCoder/SRM669-D2-500.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+class CombiningSlimes
+{
+public:
+    int maxMascots(vector <int> a)
+    {
+        sort(all(a));
+        int ret = 0;
+        while(sz(a) != 1)
+        {
+            int x = a.back();
+            a.pop_back();
+            int y = a.back();
+            a.pop_back();
+            ret += (x*y);
+            a.push_back(x+y);
+        }
+        return ret;
+    }
+};
