541a897857e1dee3c749359ea0106cab96b7a5f6
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-11 23:27:30 +0200

    Add files via upload

---
+++ TopCoder/SRM456-D2-500.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+class SilverDistance
+{
+public:
+    int minSteps(int sx, int sy, int gx, int gy)
+    {
+        int x = abs(sx-gx), y = abs(sy-gy);
+        if (x%2 == y%2) return max(x, y);
+        return max(x, abs(sy+1 - gy)) + 1;
+    }
+};
