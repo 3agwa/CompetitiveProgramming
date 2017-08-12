cedaa18f458567fd38bfd09c602d490dc007b769
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-12 16:16:19 +0200

    Update PKU 3071.cpp

--- PKU/PKU 3071.cpp
+++ PKU/PKU 3071.cpp
@@ -1,67 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
-#define vll vector<ll>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define vs vector<string>
-#define vb vector<bool>
-#define vi vector<int>
-#define vd vector<double>
-#define vvi vector< vector<int> >
-#define vvii vector< vector< pii > >
-#define ld long double
-#define mapii map<int, int>
-#define mapsi map<string, int>
-#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
-#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
-//#define INF  LLONG_MAX
-#define all(v) ((v).begin()), ((v).end())
-#define sz(v) ((int)((v).size()))
-#define pie  acos(-1)
-#define mod(n,m) ((n % m + m) % m)
-#define eps (1e-8)
-#define reset(n, m) memset(n, m, sizeof n)
-#define endl '\n'
-#define output freopen("output.txt", "w", stdout)
-#define mp(x, y, z) {x, {y, z}}
-
-double prob[1001], memo[1001][1001];
-int n, k;
-bool visited[1001][1001];
-
-double solve(int idx, int hoba)
-{
-    if (idx == n) return 1LL*hoba*100 >= 1LL*n*k;
-    if (visited[idx][hoba]) return memo[idx][hoba];
-    visited[idx][hoba] = true;
-    double ret = solve(idx+1, hoba) * (1.0 - prob[idx]) + solve(idx+1, hoba+1) * prob[idx];
-    return memo[idx][hoba] = ret;
-}
+double memo[8][(1<<7) + 1];
+double p[(1<<7) + 1][(1<<7) + 1];
+int n;
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-    rep(i, 0, n)
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    while(cin >> n && n != -1)
     {
-        ull l, r;
-        cin >> l >> r;
-        ull curr = 0;
-        for(ull j = 1; j<=r; j*=10)
+        //cout << (0^1) << " " << (1^1) << endl;
+        int m = (1<<n);
+        for(int i = 0; i<m; i++) for(int j = 0; j<m; j++) cin >> p[i][j];
+        memset(memo, 0, sizeof memo);
+        for(int i = 0; i<m; i++) memo[0][i] = 1; // base case
+        for(int i = 1; i<=n; i++)
         {
-            ll st = max(l, j);
-            ll en = min(r, 2*j - 1);
-            if (st <= en) curr += (en-st+1);
+            for(int j = 0; j<m; j++)
+            {
+                for(int k = 0; k<m; k++)
+                {
+                    if((j>>(i-1)^1)==(k>>(i-1))) // two bits are not the same
+                    {
+                        memo[i][j] += memo[i-1][j]*memo[i-1][k]*p[j][k];
+                    }
+                }
+            }
         }
-        prob[i] = 1.0 * curr / (r-l+1);
+        double mx = -1;
+        int idx;
+        for(int i = 0; i<m; i++)
+        {
+            if (memo[n][i] > mx)
+            {
+                mx = memo[n][i];
+                idx = i+1;
+            }
+        }
+        cout << idx << endl;
     }
-    cin >> k;
-    cout << fixed << setprecision(9) << solve(0, 0);
-
     return 0;
 }
