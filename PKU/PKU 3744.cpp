869b8c8b3ec48d06d04bc196e7419bbd6288531e
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-13 03:21:29 +0200

    Update PKU 3744.cpp

--- PKU/PKU 3744.cpp
+++ PKU/PKU 3744.cpp
@@ -1,67 +1,52 @@
-#include <bits/stdc++.h>
+/*
+    first thing is to compress our path, since elements can reach upto 1e8, we should compress this path
+    then we iterate over the points, if it's not a mine then we could calculate its probability
+    probabilities are usual pick/leave with (p, 1-p)
+*/
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-#define ll long long
-#define ull unsigned long long
-#define vll vector<ll>
-#define pii pair<int, int>
-#define vii vector<pii>
-#define vs vector<string>
-#define vb vector<bool>
-#define vi vector<int>
-#define vd vector<double>
-#define vvi vector< vector<int> >
-#define vvii vector< vector< pii > >
-#define ld long double
-#define mapii map<int, int>
-#define mapsi map<string, int>
-#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
-#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
-//#define INF  LLONG_MAX
-#define all(v) ((v).begin()), ((v).end())
-#define sz(v) ((int)((v).size()))
-#define pie  acos(-1)
-#define mod(n,m) ((n % m + m) % m)
-#define eps (1e-8)
-#define reset(n, m) memset(n, m, sizeof n)
-#define endl '\n'
-#define output freopen("output.txt", "w", stdout)
-#define mp(x, y, z) {x, {y, z}}
-
-double prob[1001], memo[1001][1001];
-int n, k;
-bool visited[1001][1001];
-
-double solve(int idx, int hoba)
-{
-    if (idx == n) return 1LL*hoba*100 >= 1LL*n*k;
-    if (visited[idx][hoba]) return memo[idx][hoba];
-    visited[idx][hoba] = true;
-    double ret = solve(idx+1, hoba) * (1.0 - prob[idx]) + solve(idx+1, hoba+1) * prob[idx];
-    return memo[idx][hoba] = ret;
-}
+double p, memo[502];
+int n, arr[12], mx;
+bool mine[502];
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-    rep(i, 0, n)
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+    while(cin >> n >> p)
     {
-        ull l, r;
-        cin >> l >> r;
-        ull curr = 0;
-        for(ull j = 1; j<=r; j*=10)
-        {
-            ll st = max(l, j);
-            ll en = min(r, 2*j - 1);
-            if (st <= en) curr += (en-st+1);
-        }
-        prob[i] = 1.0 * curr / (r-l+1);
-    }
-    cin >> k;
-    cout << fixed << setprecision(9) << solve(0, 0);
+        for(int i = 1; i<=n; i++) cin >> arr[i];
+        sort(arr+1, arr+n+1);
 
+        for(int i = 0; i<n; i++)
+        {
+            if (arr[i+1]-arr[i] > 50)
+            {
+                for(int j = n; j>i; j--) arr[j] -= (arr[i+1] - arr[i] - 50);
+            }
+        }
+        mx = arr[n];
+        memset(mine, false, sizeof mine);
+        memset(memo, false, sizeof memo);
+        for(int i = 1; i<=n; i++) mine[arr[i]] = true;
+        memo[1] = true;
+        for(int i = 0; i<=mx; i++)
+        {
+            if (!mine[i])
+            {
+                memo[i+1] += memo[i] * p;
+                memo[min(i+2, mx+1)] += memo[i] * (1. - p);
+            }
+        }
+        cout << fixed << setprecision(7) << memo[mx+1] << endl;
+    }
     return 0;
 }
