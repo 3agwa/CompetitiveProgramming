81be6ff5b428ce53853a1b5cd0d3a36f329ea900
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-18 04:42:17 +0200

    Add files via upload

---
+++ TopCoder/SRM693-D2-500.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {{x, y}, z}
+
+class BiconnectedDiv2
+{
+public:
+    int minimize(vector <int> w1, vector <int> w2)
+    {
+        int ret = w1.front() + w1.back();
+        rep(i, 0, sz(w2)) ret += w2[i];
+        rep(i, 1, sz(w1)-1) if (w1[i] < 0) ret += w1[i];
+        return ret;
+    }
+};
