babdd0d3822584713ca88a9a5e7ada7e6e4a0364
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-08-25 11:40:44 +0200

    Update CF626-D12-D.cpp

--- CodeForces/CF626-D12-D.cpp
+++ CodeForces/CF626-D12-D.cpp
@@ -1,3 +1,8 @@
+/*
+    the idea is to count the difference between winScore and loseScore in each round
+    from this we can run an N^2 solution, if andrew won matches I and J, then Jerry 
+    can win the rest of the games, we'll also keep track how many times I and J were included in a triplet
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -29,31 +34,50 @@ using namespace std;
 #define output freopen("output.txt", "w", stdout)
 #define mp(x, y, z) {x, {y, z}}
 
-bool cmp(pair< int, double > a, pair< int, double > b)
-{
-    return b.first*b.second*(1.-a.second) < (1.-b.second)*a.first*a.second;
-}
+ll accum[5005];
+int cnt[5005];
 
-int n;
-vector<pair< int, double > > vec;
+ll sum(int l, int r)
+{
+	if (l > r) return 0;
+	if (!l) return accum[r];
+	return accum[r] - accum[l-1];
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    int n;
     cin >> n;
-    vec = vector<pair< int, double > >(n);
-    rep(i, 0, n) cin >> vec[i].first >> vec[i].second, vec[i].second /= 100.0;
-    sort(all(vec), cmp);
-    double sum = 0, ratioo = 0;
+    vi arr(n);
+    rep(i, 0, n) cin >> arr[i];
+    sort(all(arr));
+    double deno = 0;
     rep(i, 0, n)
     {
-        //cout << vec[i].first << " " << vec[i].second << endl;
-        sum += vec[i].first;
-        sum += ratioo*(1. - vec[i].second);
-        ratioo += vec[i].first*vec[i].second;
+    	rep(j, i+1, n)
+		{
+    		cnt[arr[j]-arr[i]]++;
+
+    		deno++;
+		}
     }
-    cout << fixed << setprecision(9) << sum << endl;
+
+    erep(i, 1, 5000) accum[i] = accum[i-1] + cnt[i];
+    double ret = 0.;
+    erep(i, 1, 5000)
+    {
+    	erep(j, 1, 5000)
+		{
+    		if (i+j+1 > 5000 || !cnt[i] || !cnt[j]) continue;
+    		double temp = cnt[i] / deno;
+    		temp *= (cnt[j] / deno);
+    		temp *= (sum(i+j+1, 5000) / deno);
+    		ret += temp;
+		}
+    }
+    cout << fixed << setprecision(9) << ret << endl;
 
     return 0;
 }
