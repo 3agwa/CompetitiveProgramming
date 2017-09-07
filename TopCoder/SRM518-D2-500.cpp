085d323d6bb0080a2e0033d772e721029827c79a
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-09-07 22:08:23 +0200

    Create SRM518-D2-500.cpp

---
+++ TopCoder/SRM518-D2-500.cpp
@@ -0,0 +1,58 @@
+/*
+    greedily pick the greatest letter (in descending order)
+    keep track of the index afterwards to start over from this position
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+class LargestSubsequence
+{
+public:
+	string getLargest(string s)
+	{
+		string ret = "";
+		int idx = 0;
+		for(int i = 'z'; i >= 'a'; i--)
+		{
+			for(int j = idx; j<sz(s); j++)
+			{
+				if (s[j] == i)
+				{
+					ret.push_back(s[j]);
+					idx = j + 1;
+				}
+			}
+		}
+		return ret;
+	}
+};
+
+
