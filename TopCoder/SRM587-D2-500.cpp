8b93fd799e0f5e9d3566f1d2aa33ef6422b0d30b
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-09-07 21:58:54 +0200

    Create SRM587-D2-500.cpp

---
+++ TopCoder/SRM587-D2-500.cpp
@@ -0,0 +1,51 @@
+/*
+  simulate the steps taken, if any one lead to us being on a bad step
+  then we shouldn't have jumped in our first turn
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+class JumpFurther
+{
+public:
+	int furthest(int N, int badStep)
+	{
+		int curr = 0;
+		bool step = false;
+		erep(i, 1, N)
+		{
+			curr += i;
+			step |= (curr == badStep);
+		}
+		return curr - step;
+	}
+};
+
