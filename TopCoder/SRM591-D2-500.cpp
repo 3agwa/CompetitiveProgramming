0c5eca32c8d5390e11896da99b5e428b868917b1
Abdelrahman Mounir <a.mounir.112@gmail.com>
2017-09-11 01:29:35 +0200

    SRM591-D2-500.cpp

---
+++ TopCoder/SRM591-D2-500.cpp
@@ -0,0 +1,59 @@
+/*
+    since the input is limited by 9 letters, we can link every letter to a curtain permutation of the 9 letters
+    minimize over the pairs that don't match
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+//#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-9)
+#define reset(n, m) memset(n, m, sizeof n)
+#define endl '\n'
+#define output freopen("output.txt", "w", stdout)
+#define mp(x, y, z) {x, {y, z}}
+
+class ConvertibleStrings
+{
+public:
+	int leastRemovals(string A, string B)
+	{
+		vector<char> vec;
+		erep(i, 'A', 'I') vec.push_back(i);
+		int ret = INT_MAX;
+		int mp[125] = {};
+		do
+		{
+			erep(i, 'A', 'I') mp[i] = vec[i-'A'];
+			int cnt = 0;
+			rep(i, 0, sz(A))
+			{
+				if (B[i] != mp[A[i]]) cnt++;
+			}
+			ret = min(ret, cnt);
+		}
+		while(next_permutation(all(vec)));
+
+		return ret;
+	}
+};
